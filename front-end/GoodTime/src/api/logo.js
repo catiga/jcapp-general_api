b466b0aa2d54407f85255324467dce9dbc7e89aa
zhipeng.huo <546879387@qq.com>
2018-11-07 19:22:32 +0800

    fix:

--- front-end/GoodTime/src/api/logo.js
+++ front-end/GoodTime/src/api/logo.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const userData = function() {
-    let url = '/general_api/api/ro_code_pay?ts='+Date.parse(new Date());
+    let url = '/general_api/api/project_by_id?ts='+Date.parse(new Date());
     let obj = {};
     return axios.get(url).then(res => {
         console.log(res);
--- front-end/GoodTime/src/common/global.js
+++ front-end/GoodTime/src/common/global.js
@@ -1,6 +1,6 @@
 exports.install = function (Vue, options) {
     Vue.prototype.logo = function (){//全局函数1
-        let url = '/general_api/api/ro_code_pay?ts='+Date.parse(new Date());
+        let url = '/general_api/api/project_by_id?ts='+Date.parse(new Date());
         fetch(url).then(r => r.json()).then(d => {
             console.log('global varibal');
             console.log(d);
