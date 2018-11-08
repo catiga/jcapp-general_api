bdf45fc393ea1e83846e29ef37f327df936312cc
zhipeng.huo <546879387@qq.com>
2018-11-08 15:55:05 +0800

    docs(import iview)

--- front-end/GoodTime/src/main.js
+++ front-end/GoodTime/src/main.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import iView from 'iview'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import {fetch as fetchPolyfill} from 'whatwg-fetch'
 
 import './assets/js/rem'
 
+//import './assets/iconfont/iconfont.css'
 import 'swiper/dist/css/swiper.css'
 
 import filters from '@/common/filter'
+// import GLOBAL from '@/common/global.js'
 
 Object.keys(filters).forEach(key => {
 	Vue.filter(key, filters[key])
@@ -17,6 +20,7 @@ Object.keys(filters).forEach(key => {
 
 
 Vue.use(VueAwesomeSwiper)
+Vue.use(iView)
 
 
 Vue.config.productionTip = false
