71b73089cd04840c5eb53786127eb91f459d7e19
jackielee <jackielee@192.168.1.2>
2018-11-08 10:31:42 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/api/CityMovieList.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/CityMovieList.groovy
@@ -10,7 +10,7 @@ import com.jeancoder.core.http.JCRequest
 import com.jeancoder.app.sdk.source.RequestSource
 
 
-def sort_plans(def now_plans) {
+def sort_plans(def now_plans, def key) {
 	def date_sort_result = new LinkedList();
 	for(x in now_plans) {
 		if(!date_sort_result) {
@@ -20,13 +20,13 @@ def sort_plans(def now_plans) {
 		def last_one = date_sort_result.getLast();
 		def first_one = date_sort_result.getFirst();
 		
-		if(last_one['startTime']<=x['startTime']) {
-			println last_one['startTime'] + '???' + x['startTime'];
+		if(last_one[key]<=x[key]) {
+			println last_one[key] + '???' + x[key];
 			date_sort_result += x;
 		} else {
 			def sort_index = 0;
 			for(y in date_sort_result) {
-				if(x['startTime']>=y['startTime']) {
+				if(x[key]>=y[key]) {
 					sort_index++;
 				} else {
 					if(sort_index) {
@@ -47,45 +47,22 @@ def cinema_id = JC.request.param('cinema_id');
 
 def aaaa = JC.internal.call('ticketingsys', '/api/movies', [cinema_id:cinema_id]);
 
+println aaaa;
+
 try {
 	aaaa = JackSonBeanMapper.jsonToMap(aaaa);
 	def aaaa_data = aaaa['data'];
 	Iterator it = aaaa_data.iterator();
-	//真对结果日期进行排序
+	//针对结果日期进行排序
 	while(it.hasNext()) {
 		//def it_name = it.next()['name'].toString();
 		def it_next = it.next();
 		def date_list = it_next['dates'];
-		def date_sort_result = new LinkedList();
 		
 		for(x in date_list) {
-			x['plans'] = sort_plans(x['plans']);
-			
-			if(!date_sort_result) {
-				date_sort_result += x;
-			}
-			def last_one = date_sort_result.getLast();
-			def first_one = date_sort_result.getFirst();
-			
-			if(last_one['date']<=x['date']) {
-				date_sort_result += x;
-			} else {
-				def sort_index = 0;
-				for(y in date_sort_result) {
-					if(x['date']>=y['date']) {
-						sort_index++;
-					} else {
-						if(sort_index) {
-							date_sort_result.add(sort_index - 1, x);
-						} else {
-							date_sort_result.add(0, x);
-						}
-						break;
-					}
-				}
-			}
+			x['plans'] = sort_plans(x['plans'], 'startTime');
 		}
-		it_next['dates'] = date_sort_result;
+		it_next['dates'] = sort_plans(it_next['dates'], 'date');
 	}
 	aaaa['data'] = aaaa_data;
 }catch(any) {
