03256e42e151779602d6e9d00a8d10466591c659
jackielee <jackielee@192.168.1.2>
2018-11-08 12:34:59 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/ex/admin/user/login.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/ex/admin/user/login.groovy
@@ -9,6 +9,8 @@ import com.lengjiabao.general_api.ready.ypcall.GeneralPub
 def jc_name = JC.request.param('username');
 def jc_pass = JC.request.param('password');
 
+println jc_name + ":" + jc_pass;
+
 def pid = GlobalHolder.pid;
 
 SimpleAjax ret = JC.internal.call(SimpleAjax, 'project', '/auth/check_login', [jc_name:jc_name,jc_pass:jc_pass,pid:pid]);
