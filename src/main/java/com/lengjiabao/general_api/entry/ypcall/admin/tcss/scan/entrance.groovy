c91f896a375d6a0156b704670d2fc341560828d5
jackielee <jackielee@192.168.1.2>
2018-11-09 14:20:43 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/ypcall/admin/tcss/scan/entrance.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/ypcall/admin/tcss/scan/entrance.groovy
@@ -51,15 +51,13 @@ ticket_data['a_time'] = order['a_time'];
 def each = MoneyUtil.divide(order['pay_amount'].toString(), order['ticket_sum'].toString());
 if (new BigDecimal(each).compareTo(new BigDecimal(seats[0]['pub_fee'].toString())) >= 0 ) {
 	// 价格正常
-	ticket_data['total_amount'] = new BigDecimal(each).divide(new BigDecimal(100)).setScale(2).toString() ;
-	ticket_data['pay_amount'] = new BigDecimal(each).divide(new BigDecimal(100)).setScale(2).toString() ;
+	ticket_data['total_amount'] = new BigDecimal(each).setScale(2);
+	ticket_data['pay_amount'] = new BigDecimal(each).setScale(2);
 } else {
 	// 价格除以100
-	ticket_data['total_amount'] = new BigDecimal(seats[0]['pub_fee']).divide(new BigDecimal(100)).setScale(2).toString() ;
-	ticket_data['pay_amount'] = new BigDecimal(seats[0]['pub_fee']).divide(new BigDecimal(100)).setScale(2).toString() ;
+	ticket_data['total_amount'] = new BigDecimal(seats[0]['pub_fee']).setScale(2);
+	ticket_data['pay_amount'] = new BigDecimal(seats[0]['pub_fee']).setScale(2);
 }
-
-
 
 def o_seats = [];
 for(x in seats) {
@@ -72,10 +70,10 @@ for(x in seats) {
 	os['handle_fee'] = '0';
 	
 	if (new BigDecimal(each).compareTo(new BigDecimal(x['pub_fee'].toString())) >= 0 ) {
-		os['sale_fee'] = new BigDecimal(each).divide(new BigDecimal(100)).setScale(2).toString() ;
+		os['sale_fee'] = new BigDecimal(each).setScale(2);
 	} else {
 		// 价格除以100
-		os['sale_fee'] =new BigDecimal(x['pub_fee']).divide(new BigDecimal(100)).setScale(2).toString() ;
+		os['sale_fee'] = new BigDecimal(x['pub_fee']).setScale(2);
 	}
 	o_seats.add(os);
 }
@@ -89,82 +87,7 @@ ticket_data['orderRemote'] = orderRemote;
 
 doo.data = ticket_data;
 
-println "entrance_____" + JackSonBeanMapper.toJson(doo);
 return GeneralPub.success(doo);
-
-//try{
-//	HpOrderDto order = ypchandler_hp.tcss_my_order_detail_by_tvcode(tcode,vcode);
-//	if(order == null){
-//		doo.code = "order_not_found";
-//		doo.msg = "订单不存在";
-//		return doo;
-//	}
-//
-//
-//	if(order.order_status=='2900' || order.order_status=='2901') {
-//		doo.data = order;
-//		return doo
-//	}
-//
-//
-//	ypc_log.info(order.getStore_id());
-//	YpcSscOp ssc_op = ypchandler_hp.generate_ssc_op_by_store_id(order.getStore_id());
-//	if(ssc_op == null){
-//		doo.code = "connect_error";
-//		doo.msg = "链接影城失败";
-//		return doo;
-//	}
-//	SscOp remote_ssc_op = ssc_op.getSsc_op();
-//	AppPrintTicketResult ticket_info = remote_ssc_op.fetchPringInfo(ssc_op.getCinema_no(),tcode,vcode);
-//	if(!"0".equals(ticket_info.getCode())){
-//		doo.code = ticket_info.getRmCode();
-//		doo.msg = ticket_info.getRmMsg();
-//		return doo;
-//	}
-//	if("1".equals(ticket_info.getPrinted())){
-//		doo.code = "printed";
-//		doo.msg = "已取票,请出示纸质票";
-//		return doo;
-//	}
-//	AppSwitchPrintedResult ticket_print = remote_ssc_op.ticketPrint(ssc_op.getCinema_no(),tcode,vcode);
-//	if(!"0".equals(ticket_print.getCode())){
-//		doo.code = ticket_info.getRmCode();
-//		doo.msg = ticket_info.getRmMsg();
-//		return doo;
-//	}
-//	try{
-//		ypchandler_hp.tcss_scan_entrance(order.getOrder_no());
-//		InprmObj inprm_obj = new InprmObj();
-//		inprm_obj.setPk("admin.tcss.scan.entrance");
-//		inprm_obj.setSn("1513394646060");
-//		TcssOrder torder = new TcssOrder();
-//		torder.setId(order.getId());
-//
-//		for(HpOrderSeatDto seatdto : order.getO_seats()){
-//			BindDto bindDto = new BindDto();
-//			bindDto.user_id = ypchandler_hp.current_admin_user.getId();
-//			bindDto.user_name = ypchandler_hp.current_admin_user.getUsername();
-//			bindDto.seat = seatdto.getSeat_sr()+"排"+seatdto.getSeat_sc()+"坐";
-//			bindDto.plan_date = order.getPlan_date();
-//			bindDto.plan_time = order.getPlan_time();
-//			bindDto.movie_name = order.getFilm_name();
-//			bindDto.hall_name = order.getHall_name();
-//			bindDto.plan_id = order.getPlan_id();
-//			bindDto.store_name = order.getStore_name();
-//			bindDto.store_id = order.getStore_id();
-//
-//			ypchandler_hp.save_ypc_dis_data(ypc_call_project.getId(), inprm_obj, torder, JackSonBeanMapper.toJson(bindDto));
-//		}
-//	}catch(Exception tsex){
-//		tsex.printStackTrace();
-//	}
-//	doo.data = order;
-//}catch(Exception e) {
-//	doo.code = e.getMessage();
-//	doo.msg = e.getCause();
-//}
-//return doo;
-
 
 class DataObj {
 	def code;
