11c53d815031b1eed10ef59159d370c676cd6e1a
jackielee <jackielee@192.168.1.2>
2019-01-28 19:12:37 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/api/project_by_id.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/project_by_id.groovy
@@ -2,5 +2,13 @@ package com.lengjiabao.general_api.entry.api
 
 import com.jeancoder.app.sdk.JC
 import com.lengjiabao.general_api.ready.util.GlobalHolder
+import com.lengjiabao.general_api.ready.util.JackSonBeanMapper
 
-return JC.internal.call('project', '/incall/project_by_id', [pid:GlobalHolder.getPid().toString()]);
+def ret_data = JC.internal.call('project', '/incall/project_by_id', [pid:GlobalHolder.getPid().toString()]);
+
+ret_data = JackSonBeanMapper.jsonToMap(ret_data);
+
+def schema = JC.request.get().getSchema();
+ret_data['domain'] = schema + '://' + ret_data['domain'];
+
+return ret_data;
