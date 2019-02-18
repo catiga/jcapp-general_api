93626c94f0ebd03300a44f9e7ffb369bc264720d
jayp_hj <huang_jie@cpis.cn>
2019-02-18 21:58:59 +0800

    。

--- src/main/java/com/lengjiabao/general_api/ready/common/McAvailabilityStatus.groovy
+++ src/main/java/com/lengjiabao/general_api/ready/common/McAvailabilityStatus.groovy
@@ -3,6 +3,7 @@ package com.lengjiabao.general_api.ready.common
 import java.io.Serializable
 
 
+
 class McAvailabilityStatus implements Serializable {
 	
 	 boolean available;
@@ -11,56 +12,96 @@ class McAvailabilityStatus implements Serializable {
 	
 	 Object obj ;
 	 
-	 public McAvailabilityStatus() {}
 	 
-	 
-	 private McAvailabilityStatus(boolean result, String[] msgs, Object data) {
-		 this.available = result;
-		 this.messages = msgs;
-		 this.data = data;
+	 private McAvailabilityStatus(boolean available, String[] messages) {
+		 this.available = available;
+		 this.messages = messages;
 	 }
- 
-	 public boolean isAvailable() {
+	 
+	 private McAvailabilityStatus(boolean available, String[] messages, RetObj obj) {
+		 this.available = available;
+		 this.messages = messages;
+		 this.obj = obj;
+	 }
+	 
+	 public void setAvailable(boolean available) {
+		this.available = available;
+	 }
+
+	public void setMessages(String[] messages) {
+		this.messages = messages;
+	}
+
+	public void setObj(RetObj obj) {
+		this.obj = obj;
+	}
+
+	public boolean isAvailable() {
 		 return available;
 	 }
- 
-	 public void setAvailable(boolean available) {
-		 this.available = available;
-	 }
- 
+	 
 	 public String[] getMessages() {
 		 return messages;
 	 }
- 
-	 public void setMessages(String[] messages) {
-		 this.messages = messages;
+	 
+	 public Object getObj() {
+		 return obj;
 	 }
  
-	 public Object getData() {
-		 return data;
+	 public static McAvailabilityStatus notAvailable(String name) {
+		 String[] suggestions = new String[1];
+		 for (int i = 0; i < suggestions.length; i++) {
+			 suggestions[i] = name;
+		 }
+		 return new McAvailabilityStatus(false, suggestions);
 	 }
- 
-	 public void setData(Object data) {
-		 this.data = data;
+	 
+	 
+	 public static McAvailabilityStatus notAvailable(Object obj) {
+		 
+		 return new McAvailabilityStatus(false, ["",""] as String[], new RetObj() {
+			 @Override
+			 public Object getObj() {
+				 return obj;
+			 }
+		 });
 	 }
- 
+	 
+	 
+	 public static McAvailabilityStatus notAvailable(String[] messages) {
+		 return new McAvailabilityStatus(false, messages);
+	 }
+	 
+	 public static McAvailabilityStatus notAvailable(String[] messages, RetObj obj) {
+		 return new McAvailabilityStatus(false, messages, obj);
+	 }
+	 
 	 public static McAvailabilityStatus available() {
-		 return new McAvailabilityStatus(true, [] as String[], []);
+		 return AVAILABLE_INSTANCE;
+	 }
+	 
+	 public static McAvailabilityStatus available(RetObj obj) {
+		 return new McAvailabilityStatus(true, [""] as String[], obj);
+	 }
+	 
+	 public static McAvailabilityStatus available(Object obj) {
+		 
+		 return new McAvailabilityStatus(true, [""] as String[], new RetObj() {
+			 @Override
+			 public Object getObj() {
+				 return obj;
+			 }
+		 });
 	 }
 	 
 	 public static McAvailabilityStatus available(String[] messages) {
-		 return new McAvailabilityStatus(true, messages, null);
+		 return new McAvailabilityStatus(true, messages);
 	 }
 	 
-	 public static McAvailabilityStatus available(String[] messages, Object data) {
-		 return new McAvailabilityStatus(true, messages, data);
+	 public static McAvailabilityStatus available(String[] messages, RetObj obj) {
+		 return new McAvailabilityStatus(true, messages, obj);
 	 }
 	 
-	 public static McAvailabilityStatus notAvailable(String[] messages) {
-		 return new McAvailabilityStatus(false, messages, null);
-	 }
+	 private static final McAvailabilityStatus AVAILABLE_INSTANCE = new McAvailabilityStatus(true, new String[0]);
 	 
-	 public static McAvailabilityStatus notAvailable(String[] messages, Object data) {
-		 return new McAvailabilityStatus(false, messages, data);
-	 }
 }
