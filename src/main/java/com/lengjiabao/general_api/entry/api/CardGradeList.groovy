e0498175b81504c9a62baf3fec6ab8eb7c957ada
jayp_hj <huang_jie@cpis.cn>
2019-02-18 21:44:50 +0800

    .

--- src/main/java/com/lengjiabao/general_api/entry/api/CardGradeList.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/CardGradeList.groovy
@@ -39,7 +39,9 @@ if (status.obj != null && status.obj.size() != 0 && !"0".equals(status.obj.get(0
 		item.getpay =  item.least_recharge;
 		def list =  [];
 		list.add(item);
-		avai = McAvailabilityStatus.available(list);
+		avai = new McAvailabilityStatus();
+		avai.available = true;
+		avai.obj = list;
 		logger.info("CardGradeList__" + JackSonBeanMapper.toJson(avai))
 		return avai;
 	} catch (any) {
