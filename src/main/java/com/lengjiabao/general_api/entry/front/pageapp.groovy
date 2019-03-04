be1bc801bbb28ac3cd1a69182f785134be93ee92
jackielee <jackielee@192.168.1.2>
2019-03-04 23:03:37 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/front/pageapp.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/front/pageapp.groovy
@@ -1,6 +1,8 @@
 package com.lengjiabao.general_api.entry.front
 
 import com.jeancoder.app.sdk.JC
+import com.jeancoder.app.sdk.source.LoggerSource
+import com.jeancoder.core.log.JCLogger
 import com.jeancoder.core.result.Result
 
 def schema = JC.request.get().getSchema();
@@ -11,5 +13,8 @@ if(domain=='lzglxz.piaodaren.com') {
 }
 
 def redirect_url = schema + domain + '/general_api/tcss/index';
+
+JCLogger logger = LoggerSource.getLogger();
+logger.info(redirect_url);
 
 return new Result().setRedirectResource(redirect_url);
