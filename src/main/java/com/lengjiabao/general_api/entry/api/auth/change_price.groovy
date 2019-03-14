f63d188375d18046c12c5853ea55e50477e010d1
jayp_hj <huang_jie@cpis.cn>
2019-03-14 16:41:04 +0800

    打印提示

--- src/main/java/com/lengjiabao/general_api/entry/api/auth/change_price.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/auth/change_price.groovy
@@ -3,12 +3,15 @@ package com.lengjiabao.general_api.entry.api.auth
 import java.util.List
 
 import com.jeancoder.app.sdk.JC
+import com.jeancoder.app.sdk.source.LoggerSource
+import com.jeancoder.core.log.JCLogger
 import com.jeancoder.core.power.CommunicationParam
 import com.jeancoder.core.result.Result
 import com.jeancoder.core.util.JackSonBeanMapper
 import com.lengjiabao.general_api.ready.common.SimpleAjax
 import com.lengjiabao.general_api.ready.util.RemoteUtil
 
+JCLogger logger = LoggerSource.getLogger(this.class);
 def order_no = JC.request.param("order_no");
 def tnum = JC.request.param('tnum');
 def unicode = JC.request.param('unicode');
@@ -33,7 +36,7 @@ params.add(new CommunicationParam("ct",ct));
 params.add(new CommunicationParam("coupons",coupons));
 //SimpleAjax pref_ajax = RemoteUtil.connect(SimpleAjax, 'trade', '/incall/ro_preferential', params);
 SimpleAjax pref_ajax = JC.internal.call(SimpleAjax, 'trade', '/incall/ro_preferential', [tnum:tnum,unicode:unicode,ct:ct,coupons:coupons]);
-
+logger.info("change_price__" + pref_ajax.messages.length);
 if(pref_ajax.available) {
 	aaaa['data']['pay_amount'] = pref_ajax.data[0];
 } else {
