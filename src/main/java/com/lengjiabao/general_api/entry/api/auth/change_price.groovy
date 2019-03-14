6529df755ebd2c555f0545b5bdfa36654a6a7e5a
jayp_hj <huang_jie@cpis.cn>
2019-03-14 16:14:48 +0800

    返回错误信息

--- src/main/java/com/lengjiabao/general_api/entry/api/auth/change_price.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/auth/change_price.groovy
@@ -37,7 +37,8 @@ SimpleAjax pref_ajax = JC.internal.call(SimpleAjax, 'trade', '/incall/ro_prefere
 if(pref_ajax.available) {
 	aaaa['data']['pay_amount'] = pref_ajax.data[0];
 } else {
-	return pref_ajax;
+	aaaa['code'] = 0001
+	aaaa['msg'] = pref_ajax.messages[0];
 }
 
 Result result = new Result();
