5dcb12e3b5bffeae6b63549ccf0f12e8ad558088
jackielee <jackielee@192.168.101.211>
2019-03-31 11:23:20 +0800

    增加座位解锁接口

---
+++ src/main/java/com/lengjiabao/general_api/entry/ex/call/auth/release_seat_order.groovy
@@ -0,0 +1,19 @@
+package com.lengjiabao.general_api.entry.ex.call.auth
+
+import com.jeancoder.app.sdk.JC
+import com.jeancoder.core.result.Result
+import com.lengjiabao.general_api.ready.common.SimpleAjax
+import com.lengjiabao.general_api.ready.util.GlobalHolder
+
+def order_no = JC.request.param('order_no');
+def pid = GlobalHolder.pid;
+
+def  accountInfo = JC.request.get().getAttribute("_user_");
+def ap_id = accountInfo['ap_id'];
+
+Result result = new Result();
+
+def param_dic = [order_no:order_no,ap_id:ap_id, pid:pid];
+def aaaa = JC.internal.call('ticketingsys', '/api/releaseorder', param_dic);
+
+return result.setData(aaaa);
