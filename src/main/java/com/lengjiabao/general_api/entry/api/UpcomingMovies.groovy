57bd682adb100d8a24cc7bb74105ac7edfbf6759
jackielee <jackielee@192.168.100.231>
2019-04-13 10:59:39 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/api/UpcomingMovies.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/UpcomingMovies.groovy
@@ -1,25 +1,9 @@
 package com.lengjiabao.general_api.entry.api
 
 import com.jeancoder.app.sdk.JC
-import com.jeancoder.app.sdk.source.CommunicationSource
-import com.jeancoder.core.power.CommunicationParam
-import com.jeancoder.core.power.CommunicationPower
 import com.jeancoder.core.result.Result
-import com.jeancoder.core.http.JCRequest
-import com.jeancoder.app.sdk.source.RequestSource
-
-
-
-//String point = "ticketingsys";
-//String address = "/api/comming_list";
-
-//List<CommunicationParam> params = new ArrayList<CommunicationParam>();
-//
-//CommunicationPower systemCaller = CommunicationSource.getCommunicator(point);
-//def ret = systemCaller.doworkAsString(address, params);
 
 def ret = JC.internal.call('ticketingsys', '/api/comming_list', null);
-
  
 return new Result().setData(ret);
 
