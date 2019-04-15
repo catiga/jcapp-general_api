d2a7d71ea85f7370475eedc77af7e4160a0c832f
jackielee <jackielee@192.168.1.2>
2019-04-15 13:50:20 +0800

    增加传入apid给支付信息

--- src/main/java/com/lengjiabao/general_api/entry/api/auth/GetWxObj.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/auth/GetWxObj.groovy
@@ -12,15 +12,17 @@ def pwd = JC.request.param("pwd");
 def o_rem = JC.request.param('o_rem');
 
 def  accountInfo = JC.request.get().getAttribute("_user_");//获取用户信息
+
 def unicode;
 if (ct.equals("101001")) {
 	unicode = JC.request.param("unicode");
 } else {
 	unicode = accountInfo['part_id'];//获取用户信息
 }
+def ap_id = accountInfo['ap_id'];//获取用户信息
 
 def coupon_id = JC.request.param('coupon_id');
 
-def aaaa = JC.internal.call("trade", "/incall/ro_code_pay", [tnum:tnum,ct:ct,pwd:pwd,unicode:unicode,o_rem:o_rem,coupon_id:coupon_id,domain:domain])
+def aaaa = JC.internal.call("trade", "/incall/ro_code_pay", [tnum:tnum,ct:ct,pwd:pwd,unicode:unicode,o_rem:o_rem,coupon_id:coupon_id,domain:domain,ap_id:ap_id])
 return result.setData(aaaa);
 
