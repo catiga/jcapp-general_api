cd07884e6b2a6f934c45b7fc74a7c993a73a2bc3
jackielee <jackielee@192.168.1.2>
2019-04-21 17:36:12 +0800

    修改绑卡未登录提示

--- front-end/GoodTime/src/views/bindCard.vue
+++ front-end/GoodTime/src/views/bindCard.vue
@@ -135,8 +135,12 @@ export default {
               ]
             });
           } else {
+          	let err_msg = d.messages[0];
+            if(err_msg=='user_no_login') {
+            	err_msg = '您还未登录，请尝试从公众号进入个人中心绑卡操作';
+            }
             weui.dialog({
-              content: d.messages[0],
+              content: err_msg,
               buttons: [
                 {
                   label: "确定",
