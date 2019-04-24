427d894ec33fa840188ba4a0670371ed3e72985b
jackielee <jackielee@192.168.1.2>
2019-04-24 16:35:52 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/api/auth/GetAccount.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/auth/GetAccount.groovy
@@ -1,18 +1,22 @@
 package com.lengjiabao.general_api.entry.api.auth
 
 import com.jeancoder.app.sdk.JC
+import com.jeancoder.app.sdk.source.LoggerSource
 import com.jeancoder.core.log.JCLogger
 import com.jeancoder.core.log.JCLoggerFactory
 import com.jeancoder.core.result.Result
 import com.lengjiabao.general_api.ready.util.GlobalHolder
 
-JCLogger LOGGER = JCLoggerFactory.getLogger("auth/GetAccount");
+JCLogger LOGGER = LoggerSource.getLogger();
 
 def token = GlobalHolder.token;
 def pid = GlobalHolder.pid;
 
+def  accountInfo = JC.request.get().getAttribute("_user_");//获取用户信息
+def ap_id = accountInfo['ap_id'];//获取用户信息
+
 LOGGER.info('token=' + token + ', pid=' + pid);
-def ret = JC.internal.call('crm', '/h5/p/info', [token:token,pid:pid]);
+def ret = JC.internal.call('crm', '/h5/p/info_by_apid', [apid:ap_id,pid:pid]);
 
 Result result = new Result();
 result.setData(ret);
