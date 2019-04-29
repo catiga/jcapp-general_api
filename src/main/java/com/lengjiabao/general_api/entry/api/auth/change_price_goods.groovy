d6d6599880358ca3f0e212fab4b2fa1c5c9395f5
jackielee <jackielee@192.168.1.2>
2019-04-29 14:09:17 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/api/auth/change_price_goods.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/auth/change_price_goods.groovy
@@ -1,12 +1,10 @@
 package com.lengjiabao.general_api.entry.api.auth
 
 import com.jeancoder.app.sdk.JC
-import com.jeancoder.core.power.CommunicationParam
 import com.jeancoder.core.result.Result
 import com.jeancoder.core.util.JackSonBeanMapper
 import com.lengjiabao.general_api.ready.common.SimpleAjax
 import com.lengjiabao.general_api.ready.util.GlobalHolder//获取用户信息
-import com.lengjiabao.general_api.ready.util.RemoteUtil
 
 def order_no = JC.request.param("order_no");
 def tnum = JC.request.param('tnum');
@@ -56,17 +54,6 @@ if(aaaa&&aaaa['data']) {
 		}
 	}
 }
-
-//List<CommunicationParam> params  = new ArrayList<CommunicationParam> ();
-//params.add(new CommunicationParam("tnum",tnum));
-//params.add(new CommunicationParam("unicode",unicode));
-//params.add(new CommunicationParam("ct",ct));
-//params.add(new CommunicationParam("coupons",coupons));
-//SimpleAjax pref_ajax = RemoteUtil.connect(SimpleAjax, 'trade', '/incall/ro_preferential', params);
-//
-//if(pref_ajax.available) {
-//	aaaa['data']['pay_amount'] = pref_ajax.data[0];
-//}
 
 JC.internal.call(SimpleAjax, 'trade', '/incall/ro_preferential', ['tnum':tnum,'unicode':unicode,'ct':ct,'coupons':coupons]);
 
