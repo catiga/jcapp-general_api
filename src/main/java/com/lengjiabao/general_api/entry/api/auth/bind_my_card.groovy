f71acc7088d1ba51e2b7e06c1e88af9191f7ecd0
jackielee <jackielee@192.168.0.103>
2019-05-02 16:17:41 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/api/auth/bind_my_card.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/api/auth/bind_my_card.groovy
@@ -1,6 +1,5 @@
 package com.lengjiabao.general_api.entry.api.auth
 
-
 import com.jeancoder.app.sdk.JC
 import com.jeancoder.app.sdk.source.LoggerSource
 import com.jeancoder.core.log.JCLogger
@@ -9,12 +8,15 @@ import com.lengjiabao.general_api.ready.util.GlobalHolder
 
 JCLogger logger = LoggerSource.getLogger();
 // 绑定会员卡
-def   s_id = JC.request.param("s_id");
-def   mc_num = JC.request.param("mc_num");
-def   mc_pwd = JC.request.param("mc_pwd");
-def   pid = GlobalHolder.pid.toString();
+def s_id = JC.request.param("s_id");
+def mc_num = JC.request.param("mc_num");
+def mc_pwd = JC.request.param("mc_pwd");
+def pid = GlobalHolder.pid.toString();
 
 def accountInfo = JC.request.get().getAttribute("_user_");//获取用户信息
+if(accountInfo==null) {
+	return SimpleAjax.notAvailable('user_not_login,请先登录');
+}
 def ap_id = accountInfo['ap_id'];//获取用户信息
 def mobile = accountInfo['mobile'];
 
