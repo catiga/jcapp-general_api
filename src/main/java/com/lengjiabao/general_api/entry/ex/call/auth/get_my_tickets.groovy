97a6656a3bd42818abc2619a7fab31d2080c1fbf
jackielee <jackielee@192.168.0.103>
2019-05-08 09:25:31 +0800

    f

--- src/main/java/com/lengjiabao/general_api/entry/ex/call/auth/get_my_tickets.groovy
+++ src/main/java/com/lengjiabao/general_api/entry/ex/call/auth/get_my_tickets.groovy
@@ -1,23 +1,20 @@
-package com.lengjiabao.general_api.entry.api.auth
+package com.lengjiabao.general_api.entry.ex.call.auth
 
 import com.jeancoder.app.sdk.JC
 import com.jeancoder.app.sdk.source.LoggerSource
 import com.jeancoder.core.log.JCLogger
-import com.jeancoder.core.result.Result
-import com.lengjiabao.general_api.ready.common.SimpleAjax
 import com.lengjiabao.general_api.ready.util.GlobalHolder
+import com.lengjiabao.general_api.ready.util.JackSonBeanMapper
+import com.lengjiabao.general_api.ready.ypcall.GeneralPub
 
 JCLogger logger = LoggerSource.getLogger();
-
-Result result = new Result();
 
 def token = JC.request.param("token");
 
 def accountInfo = JC.request.get().getAttribute("_user_");//获取用户信息
 
 if(accountInfo==null) {
-	result.setData(SimpleAjax.notAvailable('token_invalid,请登录'));
-	return result;
+	return GeneralPub.fail('user_not_login', '请登录', null);
 }
 
 def ap_id = accountInfo['ap_id'];//获取用户信息
@@ -27,6 +24,12 @@ logger.info('query ticket op : ap_id=' + ap_id);
 def pid = GlobalHolder.pid;
 def aaaa = JC.internal.call('ticketingsys', '/api/orders', ['apid':ap_id,'token':token, 'pid':pid]);
 
-return result.setData(aaaa);
+def ret_data = [];
+try {
+	aaaa = JackSonBeanMapper.jsonToMap(aaaa);
+	ret_data = aaaa['data'];
+} catch(any) {}
+
+return GeneralPub.success(ret_data);
 
 
